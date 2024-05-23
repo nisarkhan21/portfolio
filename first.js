1621e0e3e545316634cc74c8c0cb01485a068891
Nisar Ahmad <nisarkhaan42@gmail.com>
2024-05-22 23:45:19 -0700

    Add files via upload
    it's my personal portfolio

---
+++ first.js
@@ -0,0 +1,35 @@
+
+var typed= new Typed(".text", {
+strings: ["Web Developer", "Freelancer", "Frontend Developer"],
+typeSpeed: 100,
+backSpeed: 100,
+backDelay: 1000,
+loop:true
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
